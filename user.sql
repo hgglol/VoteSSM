14f675c4dde0b6017d073c73a2eee499e112f867
Hgg <heyupei1996@sohu.com>
2018-06-10 21:15:06 +0800

    没有JSP User表做好

---
+++ user.sql
@@ -0,0 +1,43 @@
+﻿create database votessm;
+USE votessm;
+drop table IF EXISTS user;
+create table user(
+userId varchar(36) NOT NULL,
+loginAccount varchar(255),
+loginPassword varchar(255),
+bindEmail varchar(512),
+bindMobile varchar(11),
+nickName varchar(45),
+avatarUrl varchar(1024),
+userType int(11),
+userState int(11),
+thirdPartyOpenId varchar(255),
+thirdPartyType varchar(45),
+shortId int(11) NOT NULL,
+recordState int(11),
+createTime datetime,
+updateTime datetime,
+lastOperator varchar(36),
+lastOperatorIp varchar(45),
+PRIMARY key (userId) 
+);
+insert into user(
+userId,
+loginAccount,
+loginPassword,
+bindEmail,
+bindMobile,
+nickName,
+avataUrl,
+userType,
+userState,
+thirdPartyOpenId,
+thirdPartyType,
+shortId,
+recordState,
+createTime,
+updateTime,
+lastOperator,
+lastOpeeratorIp) values(
+
+);
